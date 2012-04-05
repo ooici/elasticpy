e9a8c9410a03b97351eee8eb57e9e78f49ffae0f
Luke Campbell <luke.s.campbell@gmail.com>
2012-04-05 14:44:54 -0400

    Luke: Finished filters

--- elasticpy.py
+++ elasticpy.py
@@ -3,7 +3,12 @@
 '''
 @author Luke Campbell <LCampbell@ASAScience.com>
 @file elasticpy.py
+@license Apache 2.0
 @description Wrapper for elastic search
+
+Copyright 2012 Lucas Campbell
+Apache License 2.0
+See COPYING for more information.
 '''
 
 import json
@@ -357,5 +362,147 @@ Filters documents matching the provided document / mapping type. Note, this filt
         self['geo_distance_range']['from'] = from_distance
         self['geo_distance_range'][field] = center
         return self
+
+    def geo_polygon(self, field, points=[]):
+        '''
+        http://www.elasticsearch.org/guide/reference/query-dsl/geo-polygon-filter.html
+        A filter allowing to include hits that only fall within a polygon of points.
+        Points are in geo-point format
+
+        > filter = ElasticFilter().geo_polygon('pin.location', [[40, -70], [30, -80], [20, -90]])
+        '''
+        if not (field and points): return
+
+        self['geo_polygon'] = {field : dict(points=points)}
+
+        return self
+
+    def has_child(self, child_type, query):
+        '''
+        http://www.elasticsearch.org/guide/reference/query-dsl/has-child-filter.html
+        The has_child filter accepts a query and the child type to run against, and results in parent documents that have child docs matching the query.
+        
+        > query = ElasticQuery().term(tag='something')
+        > filter = ElasticFilter().has_child('blog_tag', query)
+
+        '''
+
+        if not (child_type and query): return
+
+        self['has_child'] = dict(query=query)
+        self['has_child']['type'] = child_type
+        return self
+
+    def match_all(self):
+        '''
+        http://www.elasticsearch.org/guide/reference/query-dsl/match-all-filter.html
+        A filter that matches on all documents.
+        > filter = ElasticFilter().match_all()
+        '''
+
+        self['match_all'] = dict()
+        return self
+
+    def missing(self, field):
+        '''
+        http://www.elasticsearch.org/guide/reference/query-dsl/missing-filter.html
+        Filters documents where a specific field has no value in them.
+
+        '''
+        self['missing'] = dict(field=field)
+        return self
+
+    def not_filter(self, query):
+        '''
+        http://www.elasticsearch.org/guide/reference/query-dsl/not-filter.html
+        A filter that filters out matched documents using a query. This filter is more performant then bool filter. Can be placed within queries that accept a filter.
+            
+        '''
+        if not query: return
+        self['not'] = query
+        return self
+
+    def numeric_range(self,field, from_value, to_value, include_lower=True, include_upper=False):
+        '''
+        http://www.elasticsearch.org/guide/reference/query-dsl/numeric-range-filter.html
+        Filters documents with fields that have values within a certain numeric range. Similar to range filter, except that it works only with numeric values, and the filter execution works differently.
+        '''
+        if not (field, from_value and to_value): return
+
+        self['numeric_range'] = {field : { 'from' : from_value, 'to' : to_value, 'include_lower' : include_lower, 'include_upper' : include_upper } } 
+        return self
+
+    def or_filter(self, query):
+        '''
+        http://www.elasticsearch.org/guide/reference/query-dsl/or-filter.html
+
+        A filter that matches documents using OR boolean operator on other queries. This filter is more performant then bool filter. Can be placed within queries that accept a filter.
+
+        > term1 = ElasticQuery().term(lastname='Campbell')
+        > term2 = ElasticQuery().term(firstname='Luke')
+        > filter = ElasticFilter().or_filter([term1, term2])
+        '''
+
+        self['or'] = query
+        return self
+
+
+    def prefix(self, field, pre):
+        '''
+        http://www.elasticsearch.org/guide/reference/query-dsl/prefix-filter.html
+        Filters documents that have fields containing terms with a specified prefix (not analyzed). Similar to phrase query, except that it acts as a filter. Can be placed within queries that accept a filter.
+        '''
+
+        if not (field and pre): return
+        self['prefix'] = {field : pre}
+        return self
+
+    def query(self, query):
+        '''
+        http://www.elasticsearch.org/guide/reference/query-dsl/query-filter.html
+        Wraps any query to be used as a filter. Can be placed within queries that accept a filter.
+        '''
+        if not query: return
+        self['query'] = dict(query_string=query)
+        return self
+
+    def range(self, field, from_value, to_value, include_lower=True, include_upper=False):
+        '''
+        http://www.elasticsearch.org/guide/reference/query-dsl/range-filter.html
+
+        Filters documents with fields that have terms within a certain range. Similar to range query, except that it acts as a filter. Can be placed within queries that accept a filter.
+        '''
+
+        if not (field and from_value and to_value): return
+        self['range'] = {field : { 'from' : from_value, 'to' : to_value, 'include_lower' : include_lower, 'include_upper' : include_upper}}
+
+        return self
+
+    def script(self, script):
+        '''
+        http://www.elasticsearch.org/guide/reference/query-dsl/script-filter.html
+        A filter allowing to define scripts as filters. 
+
+        > script = 'doc["num1"].value > 1'
+        > filter = ElasticFilter().script(script)
+        '''
+        if not script: return
+
+        self['script'] = dict(script=script)
+        return self
+
+    def term(self, field, value):
+        '''
+        http://www.elasticsearch.org/guide/reference/query-dsl/term-filter.html
+        Filters documents that have fields that contain a term (not analyzed). Similar to term query, except that it acts as a filter.
+        '''
+
+        if not (field and value): return
+        if isinstance(value, list):
+            self['terms'] = {field : value}
+        else:
+            self['term'] = {field : value}
+        return self
+
 
 
