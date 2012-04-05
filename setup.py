53ca8326d4d685994318e3dcf17d34da2ee206a0
Luke Campbell <luke.s.campbell@gmail.com>
2012-04-05 16:31:36 -0400

    Luke: Added setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+from distutils.extension import Extension
+from Cython.Distutils import build_ext
+
+setup(
+    name='elasticpy',
+    version='0.1',
+    description='Python Wrapper for elasticsearch'
+    author='Luke Campbell',
+    author_email='luke.s.campbell@gmail.com',
+    url='http://lukecampbell.github.com',
+    packages=['elaticpy'],
+)
